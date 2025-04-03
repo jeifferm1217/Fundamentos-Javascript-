23995c14619d436bc85d44700f481eefe447ce7b
jeifferm1217 <jeiffermosquera@gmail.com>
2025-04-02 23:35:06 -0500

    Add files via upload

---
+++ javascrip/src/script.js
@@ -0,0 +1,6 @@
+console.log("Tabla de verdad del operador && (AND)");
+console.log("| X | Y | X && Y |");
+console.log("| T | T |   T    |");
+console.log("| T | F |   F    |");
+console.log("| F | T |   F    |");
+console.log("| F | F |   F    |");
