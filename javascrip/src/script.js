fad4e84f0ae46b807039514dccd99c18c551bfca
jeifferm1217 <jeiffermosquera@gmail.com>
2025-04-07 21:56:41 -0500

    Update script.js

--- javascrip/src/script.js
+++ javascrip/src/script.js
@@ -1,6 +1,37 @@
+// Tabla de verdad del operador && (AND)
 console.log("Tabla de verdad del operador && (AND)");
 console.log("| X | Y | X && Y |");
 console.log("| T | T |   T    |");
 console.log("| T | F |   F    |");
 console.log("| F | T |   F    |");
 console.log("| F | F |   F    |");
+
+// Tabla de verdad del operador || (OR)
+console.log("\nTabla de verdad del operador || (OR)");
+console.log("| X | Y | X || Y |");
+console.log("| T | T |   T    |");
+console.log("| T | F |   T    |");
+console.log("| F | T |   T    |");
+console.log("| F | F |   F    |");
+
+// Tabla de verdad del operador ! (NOT)
+console.log("\nTabla de verdad del operador ! (NOT)");
+console.log("| X | !X |");
+console.log("| T |  F |");
+console.log("| F |  T |");
+
+// Tabla de verdad del operador ^ (XOR)
+console.log("\nTabla de verdad del operador ^ (XOR)");
+console.log("| X | Y | X ^ Y |");
+console.log("| T | T |   F   |");
+console.log("| T | F |   T   |");
+console.log("| F | T |   T   |");
+console.log("| F | F |   F   |");
+
+// Tabla de verdad del operador → (Implica: !X || Y)
+console.log("\nTabla de verdad del operador → (Implica: !X || Y)");
+console.log("| X | Y | X → Y |");
+console.log("| T | T |   T   |");
+console.log("| T | F |   F   |");
+console.log("| F | T |   T   |");
+console.log("| F | F |   T   |");
